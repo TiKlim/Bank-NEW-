2b0c1a34ea3cf9c8f36824b7c478ac7326a17b3b
TiKlim <114877716+TiKlim@users.noreply.github.com>
2023-11-13 12:27:42 +0300

    Add files via upload

---
+++ Bank/Program.cs
@@ -0,0 +1,72 @@
+﻿using Bank;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SchetInBank schet1 = new SchetInBank();
+            SchetInBank schet2 = new SchetInBank();
+
+            while (true)
+            {
+                Console.WriteLine("> Меню счетов: 1 - Создать первый счёт; 2 - Создать второй счёт; 3 - Открыть первый счёт; 4 - Открыть второй счёт; 5 - Осуществить перевод средств между существующими счетами.");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                int scheta = Convert.ToInt32(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.White;
+                if (scheta == 1)
+                {
+                    Console.WriteLine("Здравствуйте. Пожалуйста введите информацию по текущему счёту \n(Номер счёта, Фамилия, Сумма на текщем счёте):");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    schet1.Info(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(), float.Parse(Console.ReadLine()));
+                    Console.ForegroundColor = ConsoleColor.White;
+                    schet1.Out();
+                }
+                else if (scheta == 2)
+                {
+                    Console.WriteLine("Здравствуйте. Пожалуйста введите информацию по текущему счёту \n(Номер счёта, Фамилия, Сумма на текщем счёте):");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    schet2.Info(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(), float.Parse(Console.ReadLine()));
+                    Console.ForegroundColor = ConsoleColor.White;
+                    schet2.Out();
+                }
+                else if (scheta == 3)
+                {
+                    schet1.Otk();
+                    schet1.Out();
+                }
+                else if (scheta == 4) 
+                {
+                    schet2.Otk();
+                    schet2.Out();
+                }
+                else if (scheta == 5)
+                {
+                    Console.WriteLine("Введите счёт для перевода первый(1) или второй(2).");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    int schet = Convert.ToInt32(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (schet == 1)
+                    {
+                        Console.WriteLine($"\nВведите сумму которую желаете перевести на другой счет?");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        schet1.Perenos(Convert.ToSingle(Console.ReadLine()), schet2);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (schet == 2)
+                    {
+                        Console.WriteLine($"\nВведите сумму которую желаете перевести на другой счет?");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        schet2.Perenos(Convert.ToSingle(Console.ReadLine()), schet1);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
---
+++ Bank/SchetInBank.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class SchetInBank
+    {
+        private int nomchet; //Номер счёта
+        private string? fio; //ФИО владельца счёта
+        private float sum; //Сумма на счету
+
+        public void Info(int nomchet, string? fio, float sum)
+        {
+            this.nomchet = nomchet;
+            this.fio = fio;
+            this.sum = sum;
+
+            if (nomchet < 0) //Проверка ввода номера счёта
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("! ОШИБКА !");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Read();
+            }
+            if (sum < 0) //Проверка ввода суммы 
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("! ОШИБКА !");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Read();
+            }
+            Console.WriteLine("x-----------------------------");
+            Console.WriteLine($"|Информация по текущему счёту: \n|Номер счёта: {nomchet}; \n|Фамилия: {fio}; \n|Сумма на счету: {sum}.");
+            Console.WriteLine("x-----------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("> Счёт открыт.");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            
+        }
+        public void Out()
+        {
+            Console.WriteLine("> Меню операций Вашего счёта: 1 - Добавить деньги на счёт; 2 - Снять деньги со счёта; 3 - Закрыть счёт; 4 - Меню счетов.");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string? vybor = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            switch (vybor)
+            {
+                case "1":
+                    this.Dob(sum);
+                    break;
+                case "2":
+                    this.Umen(sum);
+                    break;
+                case "3":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("> Счёт закрыт.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "4":
+                    Console.WriteLine("");
+                    break;
+            }
+        }
+        public void Otk()
+        {
+            Console.WriteLine("x-----------------------------");
+            Console.WriteLine($"|Информация по текущему счёту: \n|Номер счёта: {nomchet}; \n|Фамилия: {fio}; \n|Сумма на счету: {sum}.");
+            Console.WriteLine("x-----------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("> Счёт открыт.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void Dob(float sum) //Положить на счёт
+        {
+            Console.WriteLine("> Сколько Вы хотите положить на счёт?");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            float dob = float.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+            if (dob < 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("! ОШИБКА !");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Read();
+            }
+            sum = dob += sum;
+            this.Info(nomchet, fio, sum);
+            this.Out();
+        }
+        public void Umen(float sum) //Снять со счёта
+        {
+            Console.WriteLine("> Сколько Вы хотите снять со счёта?");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            float umen = float.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+            if (umen < 0) //Проверки далее
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("! ОШИБКА !");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Read();
+            }
+            if (umen < sum)
+            {
+                sum -= umen;
+            }
+            else if (umen > sum)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("! ОШИБКА !");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine();
+                this.Info(nomchet, fio, sum);
+            }
+            else if (umen == sum)
+            {
+                this.Obnul(sum);
+            }
+            this.Info(nomchet, fio, sum);
+            this.Out();
+        }
+        public void Obnul(float sum) //Взять всю сумму
+        {
+            sum = sum - sum;
+            this.Info(nomchet, fio, sum);
+            this.Out();
+        }
+        public void Perenos(float perenos, SchetInBank recipient) // Совершаем перевод денег между двумя счетами
+        {
+            if (perenos <= sum) // sumPerevod - деньги для перевода
+            {
+                sum -= perenos; // отнимаем от общ.
+                recipient.sum += perenos;
+                Console.WriteLine($"На счет: {recipient.nomchet} было переведено: {perenos} рублей");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств на счете.");
+            }
+        }       
+    }
+}
